f86f8c6c5a3ab6d4f1a1b761df26547d338caa4f
Lars Eckart <lars.eckart@hey.com>
2023-11-30 09:13:34 +0200

    wip

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "all in when pair";
 
     public static int betRequest(JsonNode request) {
+        // if we have a pair, go all in
+        if (request.get("players").get("hole_cards").get(0).get("rank").equals(request.get("players").get("hole_cards").get(1).get("rank"))) {
+            return 5000;
+        }
         return 0;
     }
 
