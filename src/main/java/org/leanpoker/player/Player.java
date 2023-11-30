f571990f269324f50a6591582ba9b9c26fdbafcd
Lars Eckart <lars.eckart@hey.com>
2023-11-30 09:18:45 +0200

    wip

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,12 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "all in when pair";
+    static final String VERSION = "all in when pair, call when king or ace";
 
     public static int betRequest(JsonNode request) {
-        // if we have a pair, go all in
         if (request.get("players").get("hole_cards").get(0).get("rank").equals(request.get("players").get("hole_cards").get(1).get("rank"))) {
             return 5000;
+        }
+        // if we have king or ace, call
+        if (request.get("players").get("hole_cards").get(0).get("rank").equals("K")
+            || request.get("players").get("hole_cards").get(1).get("rank").equals("K")
+            || request.get("players").get("hole_cards").get(0).get("rank").equals("A")
+            || request.get("players").get("hole_cards").get(1).get("rank").equals("A")) {
+            return request.get("current_buy_in").asInt() - request.get("players").get("in_action").get("bet").asInt() + request.get("minimum_raise").asInt();
         }
         return 0;
     }
